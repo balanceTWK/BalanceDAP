2bad3ebeadb480c6b6f1222fa5c791312eebc81c
balanceTWK <balanceTWK@gmail.com>
2020-03-08 19:10:31 +0800

    [change] #define _MSH_PROMPT "msh " -> #define _MSH_PROMPT "BalanceDAP@MSH "

--- code_project/rt-thread/components/finsh/shell.c
+++ code_project/rt-thread/components/finsh/shell.c
@@ -103,7 +103,7 @@ int finsh_set_prompt(const char * prompt)
 
 const char *finsh_get_prompt()
 {
-#define _MSH_PROMPT "msh "
+#define _MSH_PROMPT "BalanceDAP@MSH "
 #define _PROMPT     "finsh "
     static char finsh_prompt[RT_CONSOLEBUF_SIZE + 1] = {0};
 
